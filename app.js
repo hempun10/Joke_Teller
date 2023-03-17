04945be158c45fe1a2aa4c0edfd01f43e301cbb9
Hem Pun <hemthegentlmen@gmail.com>
2023-03-17 07:10:00 +0545

    Text To speeech SDK added

---
+++ app.js
@@ -0,0 +1,10 @@
+const test =()=>VoiceRSS.speech({
+    key: '42bd633a29b54c72a991a78d595e81a7',
+    src: 'Hello, world!',
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0, 
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false
+});
